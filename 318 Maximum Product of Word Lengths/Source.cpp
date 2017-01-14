632bef01b8ba0086656a586612a41cbb1c00b9a6
Pika Ma <pika7ma@gmail.com>
2017-01-14 21:11:42 +0800

    Update Promble 318.

--- 318 Maximum Product of Word Lengths/Source.cpp
+++ 318 Maximum Product of Word Lengths/Source.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-using namespace std;
 #include <vector>
 class Solution {
 public:
-    int maxProduct(vector<string>& words) {
-
+    int maxProduct(std::vector<std::string>& words) {
+        std::vector<int> flg;
+        for (std::vector<int>::size_type i = 0; i < words.size(); ++i) {
+            int tflg = 0;
+            for (std::string::size_type j = 0; j < words[i].length(); ++j) {
+                tflg |= (1 << (words[i][j] - 'a'));
+            }
+            flg.push_back(tflg);
+        }
+        int ans = 0;
+        for (std::vector<int>::size_type i = 0; i < flg.size(); ++i) {
+            for (std::vector<int>::size_type j = i + 1; j < flg.size(); ++j) {
+                if (!(flg[i] & flg[j])) {
+                    int tans = words[i].length() * words[j].length();
+                    if (tans > ans) {
+                        ans = tans;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
 int main()
