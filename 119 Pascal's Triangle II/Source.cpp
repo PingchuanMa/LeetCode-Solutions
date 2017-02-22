3aea03b83e20b2227b8b3bb773146a59e6c48f0b
Pika Ma <pika7ma@gmail.com>
2017-02-22 14:38:06 +0800

    Update problem 119.

--- 119 Pascal's Triangle II/Source.cpp
+++ 119 Pascal's Triangle II/Source.cpp
@@ -1,15 +1,16 @@
 #include <vector>
+#include <iostream>
 class Solution {
 public:
     std::vector<int> getRow(int rowIndex) {
-        std::vector<int> ans;
-        if (rowIndex == 0) { return ans; }
-        long upper = 1;
-        long lowerL = 1;
-        for (int i = 1; i < rowIndex; upper *= ++i);
-        long lowerR = upper;
-        for (int i = 1; i < rowIndex / 2; ++i) {
-
+        std::vector<int> ans(rowIndex + 1);
+        long upper = rowIndex;
+        long lower = 1;
+        long temp = 1;
+        for (int i = 0; i <= rowIndex / 2; ++i) {
+            ans[i] = ans[rowIndex - i] = temp;
+            temp = temp * upper-- / lower++;
         }
+        return ans;
     }
 };
