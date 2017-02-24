ddcac2e17131474cc1add9aca53672fa92c53b0f
Pika Ma <pika7ma@gmail.com>
2017-02-24 13:13:17 +0800

    Add problem 4.

---
+++ 4 Median of Two Sorted Arrays/Source.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+class Solution {
+public:
+    double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
+        if ((nums1.size() + nums2.size()) & 1) {
+            int target = (nums1.size() + nums2.size())
+        }
+
+    }
+};
