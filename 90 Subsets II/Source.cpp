04194baa101d05926a54cd65e22e6310759f9e0e
Pingchuan Ma <pika7ma@gmail.com>
2017-06-20 23:12:45 +0800

    fix bugs.

--- 90 Subsets II/Source.cpp
+++ 90 Subsets II/Source.cpp
@@ -11,12 +11,12 @@ public:
     }
 
     void subsetsHelper(const int& pos, const vector<int>& data, vector<int>& path, vector<vector<int> >& ans) {
+        ans.push_back(path);
         for (auto i = pos; i < data.size(); ++i) {
             path.push_back(data[i]);
-            ans.push_back(path);
-            while (i < data.size() - 1 && data[i] == data[i + 1]) { ++i; }
             subsetsHelper(i + 1, data, path, ans);
             path.pop_back();
+            while (i < data.size() - 1 && data[i] == data[i + 1]) { ++i; }
         }
     }
 };
