d4f2143462a96d6610e8205fb07a28694a4ecca5
Pingchuan Ma <pika7ma@gmail.com>
2017-09-07 15:31:55 +0800

    Update.

--- 268 Missing Number/Source.cpp
+++ 268 Missing Number/Source.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-#define m(x, y) ((x + y) / 2)
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int l = 0, r = nums.size() - 1;
-        if (nums[0] != 0) { return 0; }
-        while (l != r) {
-            if (m(l, r) != nums[m(l, r)]) { r = m(l, r); }
-            else { l = m(l, r) + 1; }
-        }
-        return l;
+        long long sum = (nums.size() + 1) * nums.size() / 2;
+        for (auto i = 0; i < nums.size(); ++i) { sum -= nums[i]; }
+        return sum;
     }
 };
 
 int main() {
-    vector<int> nums = { 0, 2 };
+    vector<int> nums = { 0, 1, 3 };
     Solution s;
     cout << s.missingNumber(nums) << endl;
     return 0;
--- 7 Reverse Integer/Source.cpp
+++ 7 Reverse Integer/Source.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
     int reverse(int x) {
-        int ans;
-
+        if (x == 0) { return 0; }
+        bool isPositive = true;
+        if (x < 0) {
+            isPositive = false;
+            x = -x;
+        }
+        int ans = 0;
+        bool begin = false;
+        while (x > 0) {
+            int tail = x % 10;
+            if (tail > 0 || begin) {
+                ans *= 10;
+                ans += tail;
+                begin = true;
+            }
+            x /= 10;
+            if ((ans == 214748364 && x % 10 > 7) || (ans > 214748364 && x > 0)) {
+                return 0;
+            }
+        }
+        if (isPositive) { return ans; }
+        else { return -ans; }
     }
 };
