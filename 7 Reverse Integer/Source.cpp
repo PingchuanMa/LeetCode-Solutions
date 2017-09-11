cb3181e0c354d2851438d36496372406af887df6
Pingchuan Ma <pika7ma@gmail.com>
2017-09-11 08:53:44 +0800

    Update.

--- 7 Reverse Integer/Source.cpp
+++ 7 Reverse Integer/Source.cpp
@@ -1,7 +1,33 @@
+#include <iostream>
+using namespace std;
 class Solution {
 public:
     int reverse(int x) {
-        int ans;
-
+        if (x == 0) { return 0; }
+        bool isPositive = true;
+        if (x < 0) {
+            isPositive = false;
+            x = -x;
+        }
+        int ans = 0;
+        bool begin = false;
+        while (x > 0) {
+            int tail = x % 10;
+            if (tail > 0 || begin) {
+                ans *= 10;
+                ans += tail;
+                begin = true;
+            }
+            x /= 10;
+            if (ans >= 214748364 || x % 10 > 7) {
+                return 0;
+            }
+        }
+        if (isPositive) { return ans; }
+        else { return -ans; }
     }
 };
+int main() {
+    Solution s;
+    cout << s.reverse(90000) << endl;
+}
